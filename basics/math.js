f72d6632435ae932fd4837e6235cb2120191d801
VishalSh20 <140361454+VishalSh20@users.noreply.github.com>
2024-06-12 07:29:08 +0000

    math object studied

---
+++ basics/math.js
@@ -0,0 +1,56 @@
+// summary - maximum jo mathematical operations honge vo isi object ki help se honge
+//  mathematical constants- pi, e etc
+// log10,log2, ln, round,ceil,floor,cos,sin,tan,cbrt,sqrt,random,max,min
+
+// console.log(Math)
+// // euler number 
+// console.log(Math.E)
+// console.log(Math.LN10)
+// console.log(Math.LN2)
+// console.log(Math.LOG10E)
+// console.log(Math.LOG2E)
+// console.log(Math.PI)
+// console.log(Math.SQRT1_2)
+// console.log(Math.SQRT2)
+
+// // static methods
+// console.log(Math.abs(-3324))
+// console.log(Math.sin(Math.PI/2))
+
+// console.log(Math.cbrt(8))
+// console.log(Math.sqrt(2))
+
+// console.log(Math.round(3.56))
+// console.log(Math.round(3.14))
+// console.log(Math.ceil(3.14))
+// console.log(Math.floor(3.76))
+// console.log(Math.random())
+// getting a value within the range - [min,max)
+// const minVal = 1000 , maxVal = 10000;
+// const randomInteger = Math.floor(Math.random()* (maxVal-minVal) + minVal)
+// console.log(randomInteger) 
+
+// // for getting random value with max Inclusive
+// const randomIntegerTwo  = Math.floor(Math.random()* (maxVal-minVal+1) + minVal)
+// console.log(randomIntegerTwo)
+
+// // otp generate between 0000 and 9999
+// const otpNumber = Math.floor(Math.random() * 10000)
+// const otp = "0".repeat(4-Math.log10(otpNumber)).concat(String(otpNumber))
+// console.log(otp)
+
+// console.log(Math.exp(5))
+// square root of sum of squares
+// console.log(Math.hypot(1,2,5,2))
+
+// console.log(Math.max(214,124,1,35,2))
+
+// const myArray = [2,124,1242,234,123]
+// console.log(Math.max(myArray)) //array pe nahi chalta directly
+// console.log(Math.min(myArray))
+// console.log(Math.max(...myArray))
+// console.log(Math.min(...myArray))
+
+console.log(Math.sign(-5))
+
+
